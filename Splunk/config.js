d872eee363fb12c06c740b419741beaa02504288
Mauricio <mauricioasoto@gmail.com>
2026-08-07 11:45:01 -0300

    add splunk poc

--- Splunk/config.js
+++ Splunk/config.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
   port: Number(process.env.PORT || 3000),
   splunk: {
-    enabled: process.env.SPLUNK_HEC_ENABLED === 'true',
-    token: process.env.SPLUNK_HEC_TOKEN || '',
-    url: process.env.SPLUNK_HEC_URL || 'https://localhost:8088',
-    source: process.env.SPLUNK_HEC_SOURCE || 'node-todo-app',
-    sourcetype: process.env.SPLUNK_HEC_SOURCETYPE || 'node_todo_app',
-    index: process.env.SPLUNK_HEC_INDEX || 'main',
-    strictSSL: process.env.SPLUNK_HEC_STRICT_SSL !== 'false',
+    enabled: process.env.SPLUNK_HEC_ENABLED === "true",
+    token: process.env.SPLUNK_HEC_TOKEN || "",
+    url: process.env.SPLUNK_HEC_URL || "https://localhost:8088",
+    source: process.env.SPLUNK_HEC_SOURCE || "node-app",
+    sourcetype: process.env.SPLUNK_HEC_SOURCETYPE || "node_app",
+    index: process.env.SPLUNK_HEC_INDEX || "main",
+    strictSSL: process.env.SPLUNK_HEC_STRICT_SSL !== "false",
   },
 };
 
